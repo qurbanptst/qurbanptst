4a54b49089fc6549c67d5370eef06fed05d7396e
Ady Ahmadi <ady.ahmadi24@gmail.com>
2024-05-28 11:29:30 +0800

    real new

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,27 @@
-/*=============== SHOW MENU ===============*/
+// menu toggle for mobile Response
+let menuIcon = document.querySelector('.menu-icon');
+let cancelIcon = document.querySelector('.cancel-icon');
+let headerLinks = document.querySelector('.header-links');
 
+menuIcon.addEventListener('click', function () {
+  headerLinks.style.display = 'block';
+  headerLinks.style.right = '0';
+});
 
-/*=============== REMOVE MENU MOBILE ===============*/
+cancelIcon.addEventListener('click', function () {
 
+  headerLinks.style.right = '-100vh';
+  headerLinks.style.display = 'none';
+});
 
-/*=============== SHADOW HEADER ===============*/
+// theme color change script
+let themeInput = document.getElementById('theme');
+const root = document.documentElement; // Get the root element (<html>)
+const primaryThemeColor = getComputedStyle(root).getPropertyValue('--primary-theme-color');
+themeInput.value = primaryThemeColor;
 
-
-/*=============== EMAIL JS ===============*/
-
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+themeInput.addEventListener('change', function () {
+  // Get the value of the input element when it changes
+  const color = themeInput.value;
+  document.documentElement.style.setProperty('--primary-theme-color', color);
+});
